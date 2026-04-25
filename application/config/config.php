5e56ddb2d2e3b0b6af32730880613e15729a8516
gyaneswarsahu2025-max <gyaneswarsahu2025@gmail.com>
2026-04-25 12:27:55 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/cspl/';
+$config['base_url'] = 'http://localhost/cspl_git/cspl/';
 
 /*
 |--------------------------------------------------------------------------
